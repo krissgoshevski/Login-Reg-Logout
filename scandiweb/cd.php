1fc4b02860d7f3267f845273913f7c571e0e4804
Kristijan <97643470+krissgoshevski@users.noreply.github.com>
2023-05-08 09:39:28 +0200

    Delete scandiweb directory

--- scandiweb/cd.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<div class="cd" id="cd" name="cd" style="display:none">
-<label for="size">Size (MB) </label>   
-<input type="text" name="size" id="size" placeholder="#size" /> <br>
-</div>';
-?>
