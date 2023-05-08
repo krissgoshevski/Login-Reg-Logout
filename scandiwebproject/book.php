23bcbb858156d8019f890804e7769f8389adab92
Kristijan <97643470+krissgoshevski@users.noreply.github.com>
2023-05-08 09:39:46 +0200

    Delete scandiwebproject directory

--- scandiwebproject/book.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// inputs for product Book
-echo '<div class="book" id="book" name="book" style="display:none">
-<label for="weight">Weight</label>
-<input type="text" name="weight" id="weight" placeholder="#weight"/>
-</div>';
-
-?>
